21caf81bb39121bad339b540725c25ac4c508ed0
Gennarro@yandex.ru <Gennarro@yandex.ru>
2023-08-16 18:21:43 +0300

    commit for the test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if(matrix){
+    let m = matrix.reduce((a,l,i)=>{
+      if(i%2){
+          a.push(...l.reverse())
+      }else {
+          a.push(...l)
+      }
+      return a
+    },[])
+    return m
+  }else{
+    return []
+  }
+
+
+
 }
